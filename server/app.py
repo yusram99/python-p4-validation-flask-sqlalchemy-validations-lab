e2afd9f2a5216c10861bca93206966e150205317
Your Name <youremail@example.com>
2023-09-27 21:52:23 +0300

    commit message

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
-
 from models import db, Author, Post
 
 app = Flask(__name__)
@@ -14,6 +13,50 @@ db.init_app(app)
 @app.route('/')
 def index():
     return 'Validations lab'
+
+@app.route('/authors', methods=['GET', 'POST'])
+def authors():
+    if request.method == 'GET':
+        authors = Author.query.all()
+        authors_data = [author.__dict__ for author in authors]
+        return make_response(jsonify(authors_data), 200)
+
+    if request.method == 'POST':
+        data = request.get_json()
+        try:
+            new_author = Author(
+                name=data['name'],
+                phone_number=data.get('phone_number')
+            )
+            db.session.add(new_author)
+            db.session.commit()
+            return make_response(new_author.__dict__, 201)
+        except Exception as e:
+            db.session.rollback()
+            return make_response(jsonify({'error': str(e)}), 400)
+
+@app.route('/posts', methods=['GET', 'POST'])
+def posts():
+    if request.method == 'GET':
+        posts = Post.query.all()
+        posts_data = [post.__dict__ for post in posts]
+        return make_response(jsonify(posts_data), 200)
+
+    if request.method == 'POST':
+        data = request.get_json()
+        try:
+            new_post = Post(
+                title=data['title'],
+                content=data.get('content'),
+                category=data.get('category'),
+                summary=data.get('summary')
+            )
+            db.session.add(new_post)
+            db.session.commit()
+            return make_response(new_post.__dict__, 201)
+        except Exception as e:
+            db.session.rollback()
+            return make_response(jsonify({'error': str(e)}), 400)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+
 db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Author must have a name.")
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if phone_number and (not phone_number.isdigit() or len(phone_number) != 10):
+            raise ValueError("Author phone number must be exactly ten digits if provided.")
+        return phone_number
+
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
@@ -17,16 +30,39 @@ class Author(db.Model):
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
     category = db.Column(db.String)
     summary = db.Column(db.String)
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Post must have a title.")
+        return title
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if content and len(content) < 250:
+            raise ValueError("Post content must be at least 250 characters long.")
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError("Post summary cannot exceed 250 characters.")
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ('Fiction', 'Non-Fiction'):
+            raise ValueError("Post category must be either 'Fiction' or 'Non-Fiction'.")
+        return category
+
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
